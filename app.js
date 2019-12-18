5cf2f25bede78e0f8ffa81251aa482587923b1e4
IvanchikovValentin <valentinivancikov@gmail.com>
2019-12-18 18:08:21 +0300

    chore: Remove custom parser unuseless code

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 const express = require('express');
 const app = express();
-const { startParsing } = require('./parseAsXMLs');
 const { startXPathParsing } = require('./xpath');
-const { makeHtml } = require('./htmlMaker');
 const { makeXPathHtml } = require('./xpathHtmlMaker');
 const fs = require('fs');
 
 const PORT = 3000;
-
-app.get('/', async (req, res) => {
-    const parsedData = await startParsing('myDoc/ReadLiveStudent1.xml');
-    fs.createWriteStream('response.html').write(makeHtml(parsedData).replace(/[,]/g, ''));
-    
-    return fs.createReadStream('response.html').pipe(res);
-});
 
 app.get('/xpath', async (req, res) => {
     const parsedData = await startXPathParsing();
